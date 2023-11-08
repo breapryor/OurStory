c6739a4fbc27c1441686f24082eb7acd24c5b30b
Sabrina Foreman <samsimmons1738@gmail.com>
2023-11-08 13:57:04 -0500

    all changes made are from different dates.

--- Challenge2Story/Chapter 4/Chapter4.swift
+++ Challenge2Story/Chapter 4/Chapter4.swift
@@ -7,5 +7,38 @@ import Foundation
 
 func chapterFour() {
     // Your portion of the story goes here
-    print("hello world")
+    struct Person {
+        var name:String
+        var age: Int
+        var nextage: Double
+    }
+    let person1 = Person(name: "Turtle", age: 150, nextage: 151)
+  //  let character = ["turtle"]
+    let food = ["cotton candy"]
+    let beverage = ["water"]
+  //  print("hello world")
+   // }
+print("Under the sea lived a crab named \(person1.name)")
+    print(" \(person1.name) was \(person1.age) old and very wise with a long journey ahead of him")
+    print("He needed to figure out how to get from one side of the beach to the other")
+    print("The journey for a human was only five steps while for \(person1.name) it was 10 steps")
+    print("This was a major issue because \(person1.name) hasn't made a journey like this in two days")
+    print("How will \(person1.name) get to the other side of the beach with his legs instead of human legs, he thought to himself")
+    print("After time briefly passed, it was time to take the first step")
+    print(" \(person1.name) took his first step and he could feel the sun beaming on his foot")
+    print("The sun! It's hot and bright! Screamed \(person1.name)")
+    print("I have to keep going! \(person1.name) continue forward")
+    print("By the time he reached his third step he ran into a problem, there was \(food[0]) in the pathway")
+    print("\(person1.name) didn't want to waste time or make the process longer so, he ate through the \(food[0])")
+    print("After eating through the \(food[0]) he proceeded on")
+    print(" \(person1.name) was thirsty and felt like turning back after the fifth step until he stumbled upon some \(beverage[0])")
+    print (" \(person1.name) began to drinking the \(beverage[0]) but, quickly spit it out because it was too salty")
+    print("\(person1.name) was even more thirsty and began to be a little faint")
+    print(" \(person1.name) laid in the sand and began to close his eyes")
+    print(" \(person1.name) woke up and realized that he was somehow moved to the other side of the beach")
+    print("He cheered until it started to rain")
+    print("Then \(person1.name) woke up again to realize it was all a dream")
+    print("Looking sad and hopeless \(person1.name) turned back and thought to do the journey on another day")
+    print("The End")
+    
 }
